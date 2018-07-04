c4cb2b868c434d4ee31ade2fdc059e2ebc0e7b3e
CLeaR <c.housley@auckland.ac.nz>
2018-07-04 13:32:24 +1200

    Update OnTask Reporter Tools.user.js

--- OnTask Reporter Tools.user.js
+++ OnTask Reporter Tools.user.js
@@ -8,7 +8,7 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require     https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @require     https://flexiblelearning.auckland.ac.nz/javascript/filesaver.js
-// @version     2.6.7
+// @version     2.6.9
 // @grant       none
 // ==/UserScript==
 
