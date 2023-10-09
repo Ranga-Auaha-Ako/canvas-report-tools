17cbdda7b50fb5eb14ffc46387cf2eabf67bfb3c
whol019 <wenchen.hol@gmail.com>
2023-10-09 16:05:46 +1300

    Update breadcrumb.user.js

--- breadcrumb.user.js
+++ breadcrumb.user.js
@@ -7,7 +7,7 @@
 // @include     https://*/courses/*/pages/*/edit
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require     https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
-// @version     0.51
+// @version     0.52
 // @grant       none
 // ==/UserScript==
 
@@ -28,7 +28,7 @@
   var breadCrumbCode=''; 
   var breadCrumbHead=`
   <div id="breadcrumbDiv">
-    <div class="breadcrumb" style="margin: 0 2em 1em 0; padding: 1em; background: #eee; border-width: 1px; border-style: solid; border-color: #f5f5f5 #e5e5e5 #ccc; overflow: hidden;">
+    <div class="breadcrumb" style="margin: 0; padding: 1em; background: #eee; border-width: 1px; border-style: solid; border-color: #f5f5f5 #e5e5e5 #ccc; overflow: hidden;">
         <span style="font-size: 10pt;">`;
   var breadCrumbEnd = `
         </span>
