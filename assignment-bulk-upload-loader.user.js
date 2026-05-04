4d1de03a295fbb8aebc3361cc12550ab7ded1182
whol019 <wenchen.hol@gmail.com>
2026-05-05 10:47:48 +1200

    Update assignment-bulk-upload-loader.user.js
    fix zip.js link

--- assignment-bulk-upload-loader.user.js
+++ assignment-bulk-upload-loader.user.js
@@ -9,9 +9,9 @@
 // @require     https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @require     https://flexiblelearning.auckland.ac.nz/javascript/filesaver.js
 // @require     https://unpkg.com/xlsx/dist/xlsx.full.min.js
-// @require     https://raw.githubusercontent.com/gildas-lormeau/zip.js/master/dist/zip-fs-full.min.js
+// @require     https://raw.githubusercontent.com/gildas-lormeau/zip.js/refs/heads/master/dist/zip-fs.js
 // @require     https://github.com/Ranga-Auaha-Ako/canvas-report-tools/raw/refs/heads/master/assignment-bulk-upload.user.js
-// @version     0.4
+// @version     0.5
 // @grant       none
 // ==/UserScript==
 // based on code from James Jones' Canvancement https://github.com/jamesjonesmath/canvancement
